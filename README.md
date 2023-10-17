eafce5659e63a2cb0bd73bef0ca3362d8a486088
teknovizier <122484195+teknovizier@users.noreply.github.com>
2023-10-17 15:08:55 +0200

    Store config in JSON file

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ musicat-helper
 
 Rust CLI app that walks through the specified directory structure, targeting sub-directories two levels deep. It extracts and prints the band name and album details in a CSV-like format.
 
+Setting up
+-------
+
+* Rename `config-sample.json` to `config.json`
+* Set the value of `data_folder`
+
 Usage
 -------
 
-* Adjust `DATA_DIRECTORY` variable to point to your music folder
 * Run the app with `cargo run`
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 extern crate walkdir;
 use walkdir::WalkDir;
-const DATA_DIRECTORY: &str = r#"C:\Users\User\Music\"#;
+use std::fs;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    data_folder: String,
+}
 
 fn get_band_name(entry: &walkdir::DirEntry) -> Option<&str> {
     entry.path().parent()?.file_name()?.to_str()
 }
 
 fn main() {
-    for entry in WalkDir::new(DATA_DIRECTORY).into_iter().filter_map(|entry: Result<walkdir::DirEntry, walkdir::Error>| entry.ok()) {
+    // Read the config file
+    let config_str = fs::read_to_string("config.json").expect("ERROR: Failed to read config file");
+
+    // Parse and deserialize the config
+    let config: Config = serde_json::from_str(&config_str).expect("ERROR: Failed to parse the config");
+
+    for entry in WalkDir::new(config.data_folder).into_iter().filter_map(|entry: Result<walkdir::DirEntry, walkdir::Error>| entry.ok()) {
         if entry.metadata().map_or(false, |m| m.is_dir()) && entry.depth() == 2 {
             if let Some(name) = entry.file_name().to_str() {
                 if let Some((album_year, album_name)) = name.split_once('-') {
